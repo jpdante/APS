53b072ba4eecb47e37656657aae7858c04ce9a3a
Nícolas Ribeiro Marques <56075706+NicolasRMarques@users.noreply.github.com>
2020-06-06 11:22:56 -0300

    Update Main.java

--- src/br/anhembi/aps/Main.java
+++ src/br/anhembi/aps/Main.java
@@ -1,11 +1,11 @@
 package br.anhembi.aps;
 
-// *
-// * Felipe Castro Marques - 21259039
-// * João Pedro Caires dos Santos Dante - 21321117
-// * Nícolas Ribeiro Marques - 21279441
-// * Gabriel Santana Mascena - 21265810
-// *
+/*
+    Felipe Castro Marques - 21259039
+    João Pedro Caires dos Santos Dante - 21321117
+    Nícolas Ribeiro Marques - 21279441
+    Gabriel Santana Mascena - 21265810
+*/
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
